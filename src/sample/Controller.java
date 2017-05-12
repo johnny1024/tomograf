009753abe0e999e1b633aea3a7066089ce0f04eb
Jan Szeszko <jan.szeszko@student.put.poznan.pl>
2017-05-12 17:07:53 +0200

    fxml done, controller in progress

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,14 +5,45 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    private TextField image;
+    @FXML
+    private Label imageAddressErrorLabel;
+    @FXML
+    private Button loadButton;
+
+    @FXML
+    private TextField detectorNumberTextField;
+    @FXML
+    private Label imageAddressErrorLabel;
+    @FXML
+    private TextField image;
+    @FXML
+    private Label imageAddressErrorLabel;
+    @FXML
+    private TextField image;
+    @FXML
+    private Label imageAddressErrorLabel;
+    @FXML
+    private Button loadButton;
+
 
     @FXML
     private ImageView imageViewIn;
     @FXML
+    private ImageView imageViewRadar;
+    @FXML
     private ImageView imageViewSin;
     @FXML
     private ImageView imageViewOut;
+    @FXML
+    private ImageView imageViewSin2;
+    @FXML
+    private ImageView imageViewOut2;
 
 
     public void Initialize()
